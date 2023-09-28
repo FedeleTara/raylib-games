2f2cd0610246114669eb47ea4c64720366de974c
Fedele Tara <fedele.tara@gmail.com>
2023-09-28 18:34:04 +0200

    [GAME 01] - More enemies on screen!!

--- game.c
+++ game.c
@@ -15,6 +15,9 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define MAX_ENEMIES_ON_SCREEN 3
+#define ENEMY_BLUE_TTS 100 // Time To Spawn
+
 //----------------------------------------------------------------------------------
 // Types and Structures Definition
 //----------------------------------------------------------------------------------
@@ -33,7 +36,6 @@ typedef struct {
   int height;
   int width;
   double velocity;
-  double TTS;  // Time To Spawn
   double TTM;  // Time To Move
   Color color;
 } Enemy;
@@ -48,35 +50,35 @@ const int FPS = 60;
 
 const int SCREEN_HEIGHT = 450;
 const int SCREEN_WIDTH = 350;
+const char GAME_TITLE[] = "--- [Raylib - NAME_TO_DEFINE] ---";
 const int SCREEN_PADDING_TB = 10;
 const int SCREEN_PADDING_LF = 14;
 
 const int PLAYER_HEIGHT = 8;
 const int PLAYER_WIDTH = 60;
 
-const int MAX_ENEMIES_ON_SCREEN = 3;
 const int ENEMY_BLUE_HEIGHT = 28;
 const int ENEMY_BLUE_WIDTH = 15;
 const double ENEMY_BLUE_VELOCITY = 35;
-const double ENEMY_BLUE_TTS = 2.5;
 const double ENEMY_BLUE_TTM = 4.25;
 
 player_t player;
-enemy_t enemy;
+enemy_t enemies[MAX_ENEMIES_ON_SCREEN];
 
 //------------------------------------------------------------------------------------
 // Functions Declaration
 //------------------------------------------------------------------------------------
-player_t playerActionSpawn(int height, int width);
-void playerActionMove(player_t p);
+void playerActionSpawn(int height, int width);
+void playerActionMove();
 
-enemy_t enemyActionSpawn(int height, int width, double velocity, double TTS, double TTM);
-void enemyActionFall(enemy_t e);
+void enemyActionSpawn(int index, int height, int width, double velocity, double TTM);
+void enemyActionFall(int index);
 
 void Init();
 void GameLoop();
 void Update();
 void Draw();
+void FreeMemory();
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -91,6 +93,9 @@ int main(void) {
     // De-Initialization
     CloseWindow();    // Close window and OpenGL context
     
+    // Free all the previous allocated memory
+    FreeMemory();
+
     return 0;
 }
 
@@ -99,7 +104,7 @@ int main(void) {
 //------------------------------------------------------------------------------------
 
 // Player Functions ------------------------------------------------------------------
-player_t playerActionSpawn(int height, int width) {
+void playerActionSpawn(int height, int width) {
   player_t p = malloc(sizeof(Player));
 
   p->pos = (Vector2) {
@@ -111,19 +116,19 @@ player_t playerActionSpawn(int height, int width) {
   p->velocity = 4;
   p->color = RED;
 
-  return p;
+  player = p;
 }
 
-void playerActionMove(player_t p) {
-  if ( (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) && p->pos.x > SCREEN_PADDING_LF )
-    p->pos.x -= p->velocity;
+void playerActionMove() {
+  if ( (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) && player->pos.x > SCREEN_PADDING_LF )
+    player->pos.x -= player->velocity;
 
-  if ( (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) && p->pos.x < SCREEN_WIDTH - PLAYER_WIDTH - SCREEN_PADDING_LF )
-    p->pos.x += p->velocity;
+  if ( (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) && player->pos.x < SCREEN_WIDTH - PLAYER_WIDTH - SCREEN_PADDING_LF )
+    player->pos.x += player->velocity;
 }
 
 // Enemy Functions -------------------------------------------------------------------
-enemy_t enemyActionSpawn(int height, int width, double velocity, double TTS, double TTM) {
+void enemyActionSpawn(int index, int height, int width, double velocity, double TTM) {
   enemy_t e = malloc(sizeof(Enemy));
 
   int posX = GetRandomValue( SCREEN_PADDING_LF, SCREEN_WIDTH - width - SCREEN_PADDING_LF );
@@ -132,14 +137,15 @@ enemy_t enemyActionSpawn(int height, int width, double velocity, double TTS, dou
   e->height = height;
   e->width = width;
   e->velocity = velocity;
-  e->TTS = TTS;
   e->TTM = TTM;
   e->color = BLUE;
 
-  return e;
+  enemies[index] = e;
 }
 
-void enemyActionFall(enemy_t e) {
+void enemyActionFall(int index) {
+  enemy_t e = enemies[index];
+
   if (e->pos.y > SCREEN_HEIGHT - SCREEN_PADDING_TB)
     e->pos.y = SCREEN_PADDING_TB;
   else e->pos.y += e->velocity;
@@ -147,14 +153,9 @@ void enemyActionFall(enemy_t e) {
 
 // Main Game Functions ---------------------------------------------------------------
 void Init() {  // Initialization
-  player = playerActionSpawn( PLAYER_HEIGHT, PLAYER_WIDTH );
-  enemy = enemyActionSpawn(
-    ENEMY_BLUE_HEIGHT, ENEMY_BLUE_WIDTH,
-    ENEMY_BLUE_VELOCITY, ENEMY_BLUE_TTS,
-    ENEMY_BLUE_TTM
-  );
+  playerActionSpawn( PLAYER_HEIGHT, PLAYER_WIDTH );
 
-  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "--- [Raylib - NAME_TO_DEFINE] ---");
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE);
   SetTargetFPS(FPS);    // Set our game to run at 60 frames-per-second
 }
 
@@ -171,33 +172,54 @@ void Update() {
   // Player moves
   playerActionMove(player);
 
-  // Enemies spawn
-  /*int currentSpawnTime = time / enemyTTS;
-  if ( gameTime < currentSpawnTime ) {
-    enemyPos.x = GetRandomValue(
-      screenPaddingLF,
-      screenWidth - enemyWidth - screenPaddingLF
-    );
+  bool spawn = true;
+  for (int i = 0; i < MAX_ENEMIES_ON_SCREEN; i++) {
+    enemy_t e = enemies[i];
+    if (e != NULL) {  // Enemies actions
+      // Other actions...
+      // TODO
 
-    gameTime = currentSpawnTime;
-  }*/
-
-  // Enemies fall
-  if ( !fmod(gameTime, enemy->TTM) ) enemyActionFall(enemy);
+      // Enemies fall
+      if ( !fmod(gameTime, e->TTM) ) enemyActionFall(i);
+    } else if (  // Enemies spawn
+      !fmod(gameTime, ENEMY_BLUE_TTS) && spawn
+    ) {
+      enemyActionSpawn(
+        i,
+        ENEMY_BLUE_HEIGHT, ENEMY_BLUE_WIDTH,
+        ENEMY_BLUE_VELOCITY, ENEMY_BLUE_TTM
+      );
+      spawn = false;
+    }
+  }
 }
 
 void Draw() {
   BeginDrawing();
     ClearBackground(RAYWHITE);
+    // Player ---------------------------------------------------------------
     DrawRectangle(
       player->pos.x, player->pos.y,
       player->width, player->height,
       player->color
-    );    // Player
-    DrawRectangle(
-      enemy->pos.x, enemy->pos.y,
-      enemy->width, enemy->height,
-      enemy->color
-    );    // Enemy
+    );
+    // Enemies --------------------------------------------------------------
+    for (int i = 0; i < MAX_ENEMIES_ON_SCREEN; i++) {
+      enemy_t e = enemies[i];
+      if (e != NULL)
+        DrawRectangle(
+          e->pos.x, e->pos.y,
+          e->width, e->height,
+          e->color
+        );
+    }
   EndDrawing();
 }
+
+void FreeMemory() {
+  free(player);
+  for (int i = 0; i < MAX_ENEMIES_ON_SCREEN; i++) {
+    enemy_t e = enemies[i];
+    if (e != NULL) free(e);
+  }
+} 
