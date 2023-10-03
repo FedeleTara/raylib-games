355bd357c873047ada7817587be07afce0aa60c5
Fedele Tara <fedele.tara@gmail.com>
2023-10-03 19:46:59 +0200

    [GAME 01] - working score + malloc no-more

--- game.c
+++ game.c
@@ -1,6 +1,6 @@
 /*******************************************************************************************
 *
-*   raylib - standard game: FRUITS CATCHER (temporary)
+*   raylib - classic game: FRUITS CATCHER (temporary)
 *
 *   Sample game developed by Fedele Tara (@FedeleTara)
 *
@@ -15,12 +15,27 @@
 #include <stdlib.h>
 #include <math.h>
 
+//----------------------------------------------------------------------------------
+// Macro Definition
+//----------------------------------------------------------------------------------
+
 #define MAX_ENEMIES_ON_SCREEN 3
-#define ENEMY_BLUE_TTS 100 // Time To Spawn
+#define ENEMY_BLUE_TTS 100  // Time To Spawn
+
+//----------------------------------------------------------------------------------
+// Enums Definition
+//----------------------------------------------------------------------------------
+
+typedef enum {
+  BASE = 1,
+  ADVANCED = 2,
+  BOSS = 3
+} EnemyType;
 
 //----------------------------------------------------------------------------------
 // Types and Structures Definition
 //----------------------------------------------------------------------------------
+
 typedef struct {
   Vector2 size;
   Vector3 padding;
@@ -29,8 +44,7 @@ typedef struct {
   int textSize;
   Color textColor;
 
-  Vector2 infoBoxPos;
-  Vector2 infoBoxSize;
+  Rectangle infoBoxRect;
   Color infoBoxColor;
 
   Vector2 infoTimePos;
@@ -38,73 +52,66 @@ typedef struct {
   Vector2 infoScorePos;
 } ScreenInfo;
 
-typedef ScreenInfo* screenInfo_t;
-
 typedef struct {
   double velocity;
 
-  Vector2 pos;
-  Vector2 size;
-
+  Rectangle rect;
   Color color;
 } Player;
 
-typedef Player* player_t;
-
 typedef struct {
+  double ttm;
   double velocity;
-  double TTM;  // Time To Move
+  bool spawned;
 
-  Vector2 pos;
-  Vector2 size;
-
+  EnemyType type;
+  Rectangle rect;
   Color color;
 } Enemy;
-
-typedef Enemy* enemy_t;
 
 //------------------------------------------------------------------------------------
 // Global Variables Declaration
 //------------------------------------------------------------------------------------
-const char GAME_TITLE[] = "--- [FRUITS CATCHER - Made with <3 and Raylib] ---";
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-const int GAME_FPS = 60;
+const static char GAME_TITLE[] = "--- [FRUITS CATCHER - Made with <3 and Raylib] ---";
+const static int SCREEN_WIDTH = 640;
+const static int SCREEN_HEIGHT = 480;
+const static int GAME_FPS = 60;
 
-const int PLAYER_HEIGHT = 8;
-const int PLAYER_WIDTH = 60;
+const static int PLAYER_HEIGHT = 8;
+const static int PLAYER_WIDTH = 60;
 
-const int ENEMY_BLUE_HEIGHT = 28;
-const int ENEMY_BLUE_WIDTH = 15;
-const double ENEMY_BLUE_VELOCITY = 3.75;
-const double ENEMY_BLUE_TTM = 0.5;
+const static int ENEMY_BLUE_HEIGHT = 28;
+const static int ENEMY_BLUE_WIDTH = 15;
+const static double ENEMY_BLUE_TTM = 0.5;  // Time to Mode
+const static double ENEMY_BLUE_VELOCITY = 3.75;
 
-int gameTime,
-  enemySpawned,
-  time,
-  wave,
-  score = 0;
+static int gameTime = 0;
+static int enemySpawned = 0;
+static int time = 0;
+static int wave = 0;
+static int score = 0;
 
-screenInfo_t screen;
-player_t player;
-enemy_t enemies[MAX_ENEMIES_ON_SCREEN];
+static ScreenInfo screen = { 0 };
+static Player player = { 0 };
+static Enemy enemies[MAX_ENEMIES_ON_SCREEN] = { 0 };
 
 //------------------------------------------------------------------------------------
 // Functions Declaration
 //------------------------------------------------------------------------------------
-void ScreenActionInit(int height, int width);
+static void ScreenActionInit(void);
 
-void PlayerActionSpawn(int height, int width);
-void PlayerActionMove();
+static void PlayerActionSpawn(void);
+static void PlayerActionMove(void);
 
-void EnemyActionSpawn(int index, int height, int width, double velocity, double TTM);
-void EnemyActionFall(int index);
+static void EnemyActionSpawn(EnemyType type);
+static void EnemyActionFall(int index);
+static void EnemyActionCollected(int index);
 
-void Init();
-void GameLoop();
-void Update();
-void Draw();
-void FreeMemory();
+static void Init(void);
+static void GameLoop(void);
+static void Update(void);
+static void Draw(void);
+static void Unload(void);
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -120,7 +127,7 @@ int main(void) {
     CloseWindow();    // Close window and OpenGL context
     
     // Free all the previous allocated memory
-    FreeMemory();
+    Unload();
 
     return 0;
 }
@@ -130,162 +137,178 @@ int main(void) {
 //------------------------------------------------------------------------------------
 
 // Screen Functions ------------------------------------------------------------------
-void ScreenActionInit(int height, int width) {
-  screenInfo_t s = malloc(sizeof(ScreenInfo));
-  
-  int infoBoxHeight = height / 10;
-  int innerInfoWidth = width / 3;
+void ScreenActionInit(void) {
+  int infoBoxHeight = SCREEN_HEIGHT / 10;
+  int innerInfoWidth = SCREEN_WIDTH / 3;
   int marginL = (innerInfoWidth / 100) * 3;
   int marginT = infoBoxHeight / 4;
 
-  s->size = (Vector2) { width, height };
-  s->padding = (Vector3) { infoBoxHeight, width / 100, (height / 100) * 3 };  // top, left/right, bottom
-  s->background = RAYWHITE;
+  screen.size = (Vector2) { SCREEN_WIDTH, SCREEN_HEIGHT };
+  screen.padding = (Vector3) { infoBoxHeight, SCREEN_WIDTH / 100, (SCREEN_HEIGHT / 100) * 3 };  // top, left/right, bottom
+  screen.background = RAYWHITE;
 
-  s->textSize = infoBoxHeight / 2;
-  s->textColor = BLACK;
+  screen.textSize = infoBoxHeight / 2;
+  screen.textColor = BLACK;
 
-  s->infoBoxPos = (Vector2) { 0, 0 };
-  s->infoBoxSize = (Vector2) { width, infoBoxHeight};
-  s->infoTimePos = (Vector2) { marginL, marginT };
-  s->infoWavePos = (Vector2) { innerInfoWidth + marginL, marginT };
-  s->infoScorePos = (Vector2) { innerInfoWidth * 2 + marginL, marginT };
-  s->infoBoxColor = ORANGE;
-
-  screen = s;
+  screen.infoBoxRect = (Rectangle) { 0, 0, SCREEN_WIDTH, infoBoxHeight };
+  screen.infoTimePos = (Vector2) { marginL, marginT };
+  screen.infoWavePos = (Vector2) { innerInfoWidth + marginL, marginT };
+  screen.infoScorePos = (Vector2) { innerInfoWidth * 2 + marginL, marginT };
+  screen.infoBoxColor = ORANGE;
 }
 
 // Player Functions ------------------------------------------------------------------
-void PlayerActionSpawn(int height, int width) {
-  player_t p = malloc(sizeof(Player));
-
-  p->pos = (Vector2) {
-    screen->size.x / 2 - width / 2,
-    screen->size.y - height - screen->padding.z
+void PlayerActionSpawn(void) {
+  player.rect = (Rectangle) {
+    screen.size.x / 2 - PLAYER_WIDTH / 2,
+    screen.size.y - PLAYER_HEIGHT - screen.padding.z,
+    PLAYER_WIDTH, PLAYER_HEIGHT
   };
-  p->size = (Vector2) { width, height };
-  p->velocity = 4;
-  p->color = RED;
-
-  player = p;
+  player.velocity = 4;
+  player.color = RED;
 }
 
-void PlayerActionMove() {
-  if ( (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) && player->pos.x > screen->padding.y )
-    player->pos.x -= player->velocity;
+void PlayerActionMove(void) {
+  if ( (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) && player.rect.x > screen.padding.y )
+    player.rect.x -= player.velocity;
 
-  if ( (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) && player->pos.x < screen->size.x - player->size.x - screen->padding.y )
-    player->pos.x += player->velocity;
+  if ( (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) && player.rect.x < screen.size.x - player.rect.width - screen.padding.y )
+    player.rect.x += player.velocity;
 }
 
 // Enemy Functions -------------------------------------------------------------------
-void EnemyActionSpawn(int index, int height, int width, double velocity, double TTM) {
-  enemy_t e = malloc(sizeof(Enemy));
+void EnemyActionSpawn(EnemyType type) {
+  for (int i = 0; i < MAX_ENEMIES_ON_SCREEN; i++) {
+    if (!enemies[i].spawned) {
+      if (type == BASE) {
+        int posX = GetRandomValue( screen.padding.y, screen.size.x - ENEMY_BLUE_WIDTH - screen.padding.y );
 
-  int posX = GetRandomValue( screen->padding.y, screen->size.x - width - screen->padding.y );
+        enemies[i].rect = (Rectangle) {
+          posX, screen.padding.x,
+          ENEMY_BLUE_WIDTH, ENEMY_BLUE_HEIGHT
+        };
+        enemies[i].ttm = ENEMY_BLUE_TTM;
+        enemies[i].velocity = ENEMY_BLUE_VELOCITY;
+        enemies[i].color = BLUE;
 
-  e->pos = (Vector2) { posX, screen->padding.x };
-  e->size = (Vector2) { width, height };
-  e->velocity = velocity;
-  e->TTM = TTM;
-  e->color = BLUE;
+        enemies[i].spawned = true;
+      }
 
-  enemies[index] = e;
+      break;
+    }
+  }
 }
 
 void EnemyActionFall(int index) {
-  enemy_t e = enemies[index];
+  if (
+    enemies[index].rect.y <
+    screen.size.y - screen.padding.z - enemies[index].rect.height
+  )
+    enemies[index].rect.y += enemies[index].velocity;
+  else
+    enemies[index].rect.y = screen.padding.x;
+}
 
-  if (e->pos.y < screen->size.y - screen->padding.z - e->size.y)
-    e->pos.y += e->velocity;
-  else e->pos.y = screen->padding.x;
+void EnemyActionCollected(int index) {
+  enemies[index] = (Enemy) { 0 };
+  score++;
+  enemySpawned--;
 }
 
 // Main Game Functions ---------------------------------------------------------------
-void Init() {  // Initialization
-  ScreenActionInit( SCREEN_HEIGHT, SCREEN_WIDTH );
-  PlayerActionSpawn( PLAYER_HEIGHT, PLAYER_WIDTH );
+void Init(void) {  // Initialization
+  ScreenActionInit();
+  PlayerActionSpawn();
   InitWindow( SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE );
   SetTargetFPS( GAME_FPS );  // Set our game to run at 60 frames-per-second
 }
 
-void GameLoop() {  // Main game loop
+void GameLoop(void) {  // Main game loop
   while(!WindowShouldClose()) {  // Detect window close button or ESC key
     Update();
     Draw();
   }
 }
 
-void Update() {
+void Update(void) {
   time = GetTime();
   gameTime++;
 
-  PlayerActionMove(player);  // Player moves
+  // Player ---------------------------------------------------------------
+  PlayerActionMove();
 
-  for (int i = 0; i < enemySpawned; i++) { // Enemies action
-    enemy_t e = enemies[i];
-    
-    // Other actions...
-    // TODO
+  // Enemies --------------------------------------------------------------
+  for (
+    int i = 0, j = 0;
+    j < enemySpawned && i < MAX_ENEMIES_ON_SCREEN;
+    i++
+  ) {
+    if (enemies[i].spawned) {
+      // Enemies fall
+      if ( !fmod(gameTime, enemies[i].ttm) )
+        EnemyActionFall(i);
+      
+      // Other actions...
+      // TODO
 
-    // Enemies fall
-    if ( !fmod(gameTime, e->TTM) ) EnemyActionFall(i);
+      // Detect Collisions
+      if( CheckCollisionRecs(player.rect, enemies[i].rect) )
+        EnemyActionCollected(i);
+
+      // Increment the counter
+      j++;
+    }
   }
 
   if (  // Enemies spawn (for now, only blue rectangles :D)
       enemySpawned < MAX_ENEMIES_ON_SCREEN && !fmod(gameTime, ENEMY_BLUE_TTS)
     ) {
-      EnemyActionSpawn(
-        enemySpawned,
-        ENEMY_BLUE_HEIGHT, ENEMY_BLUE_WIDTH,
-        ENEMY_BLUE_VELOCITY, ENEMY_BLUE_TTM
-      );
+      EnemyActionSpawn( BASE );
       enemySpawned++;
   }
 }
 
-void Draw() {
+void Draw(void) {
   BeginDrawing();
 
-    ClearBackground( screen->background );
+    ClearBackground( screen.background );
 
     // Info -----------------------------------------------------------------
-    DrawRectangleV( screen->infoBoxPos, screen->infoBoxSize, screen->infoBoxColor );
+    DrawRectangleRec( screen.infoBoxRect, screen.infoBoxColor );
     DrawText(
       TextFormat("time %i", time),
-      screen->infoTimePos.x, screen->infoTimePos.y,
-      screen->textSize, screen->textColor
+      screen.infoTimePos.x, screen.infoTimePos.y,
+      screen.textSize, screen.textColor
     );
     DrawText(
       TextFormat("wave %i", wave),
-      screen->infoWavePos.x, screen->infoWavePos.y,
-      screen->textSize, screen->textColor
+      screen.infoWavePos.x, screen.infoWavePos.y,
+      screen.textSize, screen.textColor
     );
     DrawText(
       TextFormat("score %i", score),
-      screen->infoScorePos.x, screen->infoScorePos.y,
-      screen->textSize, screen->textColor
+      screen.infoScorePos.x, screen.infoScorePos.y,
+      screen.textSize, screen.textColor
     );
 
     // Player ---------------------------------------------------------------
-    DrawRectangleV( player->pos, player->size, player->color );
+    DrawRectangleRec( player.rect, player.color );
 
     // Enemies --------------------------------------------------------------
-    for (int i = 0; i < enemySpawned; i++) {
-      enemy_t e = enemies[i];
-      DrawRectangleV( e->pos, e->size, e->color );
+    for (
+      int i = 0, j = 0;
+      j < enemySpawned && i < MAX_ENEMIES_ON_SCREEN;
+      i++
+    ) {
+      if (enemies[i].spawned) {
+        DrawRectangleRec( enemies[i].rect, enemies[i].color );
+        j++;
+      }
     }
 
   EndDrawing();
 }
 
-void FreeMemory() {
-  free(player);
-
-  for (int i = 0; i < enemySpawned; i++) {
-    enemy_t e = enemies[i];
-    free(e);
-  }
-
-  free(screen);
-} 
+void Unload(void) {  // Unload game variables
+  // :Unload here all dynamic loaded data (textures, sounds, models...)
+}
