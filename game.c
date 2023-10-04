3cd68daeb19eb7e27e98377fd3d5a6acf4fd1987
Fedele Tara <fedele.tara@gmail.com>
2023-10-04 19:29:21 +0200

    [GAME 01] - Gameover handling

--- game.c
+++ game.c
@@ -18,24 +18,27 @@
 //----------------------------------------------------------------------------------
 // Macro Definition
 //----------------------------------------------------------------------------------
-
 #define MAX_ENEMIES_ON_SCREEN 100
 #define ENEMY_BLUE_TTS 50  // Time To Spawn
 
 //----------------------------------------------------------------------------------
 // Enums Definition
 //----------------------------------------------------------------------------------
-
 typedef enum {
   BASE = 1,
   ADVANCED = 2,
   BOSS = 3
 } EnemyType;
 
+typedef enum {
+  IN_GAME = 0,
+  OVERTIME = 1,
+  WIN = 2
+} GameState;
+
 //----------------------------------------------------------------------------------
 // Types and Structures Definition
 //----------------------------------------------------------------------------------
-
 typedef struct {
   Vector2 size;
   Vector3 padding;
@@ -72,10 +75,16 @@ typedef struct {
 //------------------------------------------------------------------------------------
 // Global Variables Declaration
 //------------------------------------------------------------------------------------
-const static char GAME_TITLE[] = "--- [FRUITS CATCHER - Made with <3 and Raylib] ---";
+const static char LABEL_TITLE[] = "--- [FRUITS CATCHER - Made with <3 and Raylib] ---";
+const static char LABEL_GAMEOVER[] = "GAME OVER";
+const static char LABEL_WIN[] = "YOU WIN";
+const static char LABEL_OVERTIME[] = "OVERTIME";
+
 const static int SCREEN_WIDTH = 640;
 const static int SCREEN_HEIGHT = 480;
 const static int GAME_FPS = 60;
+const static int MAX_TIME = 5;
+const static int MAX_SCORE = 1;
 
 const static int PLAYER_HEIGHT = 8;
 const static int PLAYER_WIDTH = 60;
@@ -91,6 +100,7 @@ static int time = 0;
 static int wave = 0;
 static int score = 0;
 
+static GameState gamestate = IN_GAME;
 static ScreenInfo screen = { 0 };
 static Player player = { 0 };
 static Enemy enemies[MAX_ENEMIES_ON_SCREEN] = { 0 };
@@ -98,14 +108,15 @@ static Enemy enemies[MAX_ENEMIES_ON_SCREEN] = { 0 };
 //------------------------------------------------------------------------------------
 // Functions Declaration
 //------------------------------------------------------------------------------------
-static void ScreenActionInit(void);
-
 static void PlayerActionSpawn(void);
 static void PlayerActionMove(void);
 
 static void EnemyActionSpawn(EnemyType type);
 static void EnemyActionFall(int index);
 static void EnemyActionCollect(int index);
+
+static void ScreenInit(void);
+static void GameOver(GameState);
 
 static void Init(void);
 static void GameLoop(void);
@@ -136,29 +147,9 @@ int main(void) {
 // Functions Definition
 //------------------------------------------------------------------------------------
 
-// Screen Functions ------------------------------------------------------------------
-void ScreenActionInit(void) {
-  int infoBoxHeight = SCREEN_HEIGHT / 10;
-  int innerInfoWidth = SCREEN_WIDTH / 3;
-  int marginL = (innerInfoWidth / 100) * 3;
-  int marginT = infoBoxHeight / 4;
-
-  screen.size = (Vector2) { SCREEN_WIDTH, SCREEN_HEIGHT };
-  screen.padding = (Vector3) { infoBoxHeight, SCREEN_WIDTH / 100, (SCREEN_HEIGHT / 100) * 3 };  // top, left/right, bottom
-  screen.background = RAYWHITE;
-
-  screen.textSize = infoBoxHeight / 2;
-  screen.textColor = BLACK;
-
-  screen.infoBoxRect = (Rectangle) { 0, 0, SCREEN_WIDTH, infoBoxHeight };
-  screen.infoTimePos = (Vector2) { marginL, marginT };
-  screen.infoWavePos = (Vector2) { innerInfoWidth + marginL, marginT };
-  screen.infoScorePos = (Vector2) { innerInfoWidth * 2 + marginL, marginT };
-  screen.infoBoxColor = ORANGE;
-}
-
 // Player Functions ------------------------------------------------------------------
 void PlayerActionSpawn(void) {
+  // TODO Play animation "Spawn Player"
   player.rect = (Rectangle) {
     screen.size.x / 2 - PLAYER_WIDTH / 2,
     screen.size.y - PLAYER_HEIGHT - screen.padding.z,
@@ -169,18 +160,27 @@ void PlayerActionSpawn(void) {
 }
 
 void PlayerActionMove(void) {
-  if ( (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) && player.rect.x > screen.padding.y )
+  if (
+    (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))
+    && player.rect.x > screen.padding.y
+  ) {
     player.rect.x -= player.velocity;
+  }
 
-  if ( (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) && player.rect.x < screen.size.x - player.rect.width - screen.padding.y )
+  if (
+    (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))
+    && player.rect.x < screen.size.x - player.rect.width - screen.padding.y
+  ) {
     player.rect.x += player.velocity;
+  }
 }
 
-// Enemy Functions -------------------------------------------------------------------
+// Enemy (main) Functions -------------------------------------------------------------------
 void EnemyActionSpawn(EnemyType type) {
   for (int i = 0; i < MAX_ENEMIES_ON_SCREEN; i++) {
     if (!enemies[i].spawned) {
       if (type == BASE) {
+        // TODO Play animation "Spawn Blue"
         int posX = GetRandomValue( screen.padding.y, screen.size.x - ENEMY_BLUE_WIDTH - screen.padding.y );
 
         enemies[i].rect = (Rectangle) {
@@ -203,9 +203,10 @@ void EnemyActionFall(int index) {
   if (
     enemies[index].rect.y <
     screen.size.y - screen.padding.z - enemies[index].rect.height
-  )
+  ) {
+    // TODO Play animation "Fall"
     enemies[index].rect.y += enemies[index].velocity;
-  else {
+  } else {
     // TODO Play animation "Rot"
     enemies[index] = (Enemy) { 0 };
     enemySpawned--;
@@ -219,23 +220,78 @@ void EnemyActionCollect(int index) {
   score++;
 }
 
-// Main Game Functions ---------------------------------------------------------------
+// Main Game (tools) Functions ---------------------------------------------------------------
+void ScreenInit(void) {
+  int infoBoxHeight = SCREEN_HEIGHT / 10;
+  int innerInfoWidth = SCREEN_WIDTH / 3;
+  int marginL = (innerInfoWidth / 100) * 3;
+  int marginT = infoBoxHeight / 4;
+
+  screen.size = (Vector2) { SCREEN_WIDTH, SCREEN_HEIGHT };
+  screen.padding = (Vector3) { infoBoxHeight, SCREEN_WIDTH / 100, (SCREEN_HEIGHT / 100) * 3 };  // top, left/right, bottom
+  screen.background = RAYWHITE;
+
+  screen.textSize = infoBoxHeight / 2;
+  screen.textColor = BLACK;
+
+  screen.infoBoxRect = (Rectangle) { 0, 0, SCREEN_WIDTH, infoBoxHeight };
+  screen.infoTimePos = (Vector2) { marginL, marginT };
+  screen.infoWavePos = (Vector2) { innerInfoWidth + marginL, marginT };
+  screen.infoScorePos = (Vector2) { innerInfoWidth * 2 + marginL, marginT };
+  screen.infoBoxColor = ORANGE;
+}
+
+void GameOver(GameState state) {
+  // TODO Play animation "Game Over"
+  player = (Player) { 0 };
+  
+  for (
+    int i = 0, j = 0;
+    j < enemySpawned && i < MAX_ENEMIES_ON_SCREEN;
+    i++
+  ) {
+    if (enemies[i].spawned) {
+      enemies[i] = (Enemy) { 0 };
+    }
+  }
+
+  gamestate = state;
+}
+
+// Main Game (essentials) Functions ---------------------------------------------------------------
 void Init(void) {  // Initialization
-  ScreenActionInit();
+  ScreenInit();
   PlayerActionSpawn();
-  InitWindow( SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE );
+  InitWindow( SCREEN_WIDTH, SCREEN_HEIGHT, LABEL_TITLE );
   SetTargetFPS( GAME_FPS );  // Set our game to run at 60 frames-per-second
 }
 
 void GameLoop(void) {  // Main game loop
-  while(!WindowShouldClose()) {  // Detect window close button or ESC key
-    Update();
+  while( !WindowShouldClose() ) {  // Detect window close button or ESC key
+    // If a game condition ends the game, then, it should stops update everything
+    if ( gamestate == IN_GAME ) {
+      Update();
+    }
+
     Draw();
   }
 }
 
 void Update(void) {
+  // Time section
   time = GetTime();
+  if ( time >= MAX_TIME ) {
+    GameOver(OVERTIME);
+    return;
+  }
+  
+  // Score section
+  if ( score >= MAX_SCORE) {
+    GameOver(WIN);
+    return;
+  }
+
+  // Everything else
   gameTime++;
 
   // Player ---------------------------------------------------------------
@@ -249,15 +305,17 @@ void Update(void) {
   ) {
     if (enemies[i].spawned) {
       // Enemies fall
-      if ( !fmod(gameTime, enemies[i].ttm) )
+      if ( !fmod(gameTime, enemies[i].ttm) ) {
         EnemyActionFall(i);
+      }
       
       // Other actions...
       // TODO
 
       // Detect Collisions
-      if( CheckCollisionRecs(player.rect, enemies[i].rect) )
+      if( CheckCollisionRecs(player.rect, enemies[i].rect) ) {
         EnemyActionCollect(i);
+      }
 
       // Increment the counter
       j++;
@@ -295,19 +353,57 @@ void Draw(void) {
       screen.textSize, screen.textColor
     );
 
-    // Player ---------------------------------------------------------------
-    DrawRectangleRec( player.rect, player.color );
+    switch ( gamestate ) {
+      case IN_GAME:
+        // Player ---------------------------------------------------------------
+        DrawRectangleRec( player.rect, player.color );
 
-    // Enemies --------------------------------------------------------------
-    for (
-      int i = 0, j = 0;
-      j < enemySpawned && i < MAX_ENEMIES_ON_SCREEN;
-      i++
-    ) {
-      if (enemies[i].spawned) {
-        DrawRectangleRec( enemies[i].rect, enemies[i].color );
-        j++;
-      }
+        // Enemies --------------------------------------------------------------
+        for (
+          int i = 0, j = 0;
+          j < enemySpawned && i < MAX_ENEMIES_ON_SCREEN;
+          i++
+        ) {
+          if (enemies[i].spawned) {
+            DrawRectangleRec( enemies[i].rect, enemies[i].color );
+            j++;
+          }
+        }
+        break;
+
+      case WIN:
+        DrawText(
+          LABEL_WIN,
+          ( screen.size.x - MeasureText(LABEL_WIN, screen.textSize) ) / 2,
+          ( screen.size.y - screen.textSize ) / 2,
+          screen.textSize, BLACK
+        );
+        break;
+      
+      case OVERTIME:
+        DrawText(
+          LABEL_GAMEOVER,
+          ( screen.size.x - MeasureText(LABEL_GAMEOVER, screen.textSize) ) / 2,
+          ( screen.size.y - screen.textSize ) / 2,
+          screen.textSize, BLACK
+        );
+        DrawText(
+          LABEL_OVERTIME,
+          ( screen.size.x - MeasureText(LABEL_OVERTIME, screen.textSize) ) / 2,
+          ( screen.size.y + screen.textSize ) / 2,
+          screen.textSize, BLACK
+        );
+        break;
+
+      default:
+        // Game Over (generic)
+        DrawText(
+          LABEL_GAMEOVER,
+          ( screen.size.x - MeasureText(LABEL_GAMEOVER, screen.textSize) ) / 2,
+          ( screen.size.y - screen.textSize ) / 2,
+          screen.textSize, BLACK
+        );
+        break;
     }
 
   EndDrawing();
